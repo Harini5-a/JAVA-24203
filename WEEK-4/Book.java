2488b5c6fe0979978eb0b30f6bcce3f874384532
Harini5-a <harinikasi5@gmail.com>
2025-02-28 12:15:30 +0530

    Add files via upload

---
+++ WEEK-4/Book.java
@@ -0,0 +1,21 @@
+public class Book {
+    String title;
+    String author;
+    int year_of_publication;
+  
+    public Book(String title, String author, int year_of_publication ) {
+        this.title = title;
+        this.author = author;
+        this.year_of_publication = year_of_publication;
+    }
+
+    public void displaydetails {
+       System.out.println(book_one.title);
+       System.out.println(book_one.author);
+       System.out.println(book_one.year_of_publication);
+       System.out.println();
+    }
+}
+    
+
+
